1ac3462e45f610fb07da9b18ffc5d31c3d601510
KevinCruzBasantes <kicruz@uce.edu.ec>
2024-06-08 16:19:16 -0500

    Creacion nivel 1

--- Galaga/src/main/java/ec/edu/uce/Galaga/models/Hero.java
+++ Galaga/src/main/java/ec/edu/uce/Galaga/models/Hero.java
@@ -10,7 +10,7 @@ public class Hero implements Drawable, Movable, Shootable {
     public int[] cord_y = {500, 550, 550};
     private int lives = 100; // Contador de vidas
     private int health = 100; // Vida del héroe
-
+int damage;
     @Override
     public void draw(Graphics graphics) {
         graphics.setColor(Color.WHITE);
@@ -63,11 +63,10 @@ public class Hero implements Drawable, Movable, Shootable {
         return heroPolygon.contains(bullet.getX(), bullet.getY());
     }
 
-    public void decreaseLife() {
+    public void decreaseLife(int opponentDamage) {
         if (lives > 0) {
-            lives-=5;
-            // Disminuir la vida del héroe en 5 cada vez que se impacta con una bala
-            health -= 5;
+            lives -= damage;
+            health -= damage;
         }
     }
 
